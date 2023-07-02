6af6c87af02930de9c15ee0739d72785f80d0da0
walidramadan12 <107707564+walidramadan12@users.noreply.github.com>
2023-07-03 00:01:32 +0300

    update
    js reset

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,20 +3,51 @@ let btn01 = document.getElementById("button_score01")
 let btn02 = document.getElementById("button_score02")
 let btn03 = document.getElementById("button_score03")
 let scoreH = document.getElementById("home-score")
-let count = 0
+
+let btn1 = document.getElementById("button_score1")
+let btn2 = document.getElementById("button_score2")
+let btn3 = document.getElementById("button_score3")
+let scoreG = document.getElementById("guest-score")
+
+let count01 = 0
+let count02 = 0
+
+let stats = document.getElementById("stats-score")
 
 function button01() {
-    count=count+1
-    console.log(count)
-    scoreH.innerHTML=count
+    count01=count01+1
+    console.log(count01)
+    scoreH.textContent=count01
 }
 function button02() {
-    count=count+2
-    console.log(count)
-    scoreH.innerHTML=count  
+    count01=count01+2
+    console.log(count01)
+    scoreH.innerHTML=count01
 }
 function button03() {
-    count=count+3
-    console.log(count)
-    scoreH.innerHTML=count  
+    count01=count01+3
+    console.log(count01)
+    scoreH.innerHTML=count01 
+}
+
+function button1() {
+    count02=count02+1
+    console.log(count02)
+    scoreG.innerHTML=count02
+}
+function button2() {
+    count02=count02+2
+    console.log(count02)
+    scoreG.innerHTML=count02
+}
+function button3() {
+    count02=count02+3
+    console.log(count02)
+    scoreG.innerHTML=count02
+}
+function reset() {
+    scoreG.innerHTML = 0
+    scoreH.innerHTML = 0 
+    count01=0
+    count02=0
 }

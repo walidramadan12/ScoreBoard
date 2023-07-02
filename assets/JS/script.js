85500ed455c8655e57a5a5a23b31657e3620a130
walidramadan12 <107707564+walidramadan12@users.noreply.github.com>
2023-07-03 01:21:28 +0300

    always updating :)
    js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -11,43 +11,66 @@ let scoreG = document.getElementById("guest-score")
 
 let count01 = 0
 let count02 = 0
+// let remainH = count01-count02
+// let remainG = count02-count01
 
 let stats = document.getElementById("stats-score")
 
 function button01() {
     count01=count01+1
-    console.log(count01)
     scoreH.textContent=count01
+    result()
 }
 function button02() {
     count01=count01+2
-    console.log(count01)
     scoreH.innerHTML=count01
+    result()
 }
 function button03() {
     count01=count01+3
-    console.log(count01)
     scoreH.innerHTML=count01 
+    result()
 }
 
 function button1() {
     count02=count02+1
-    console.log(count02)
     scoreG.innerHTML=count02
+    result()
 }
 function button2() {
     count02=count02+2
-    console.log(count02)
     scoreG.innerHTML=count02
+    result()
 }
 function button3() {
     count02=count02+3
-    console.log(count02)
     scoreG.innerHTML=count02
+    result()
 }
 function reset() {
     scoreG.innerHTML = 0
     scoreH.innerHTML = 0 
     count01=0
     count02=0
+    document.getElementById("guest").style.color="white"
+    document.getElementById("home").style.color="white"
+    stats.innerHTML = "Same Score Both"
+}
+
+function result(){
+    if(count01>count02){
+        stats.innerHTML = "home is higher by:";count01-count02
+        document.getElementById("home").style.color="red"
+        document.getElementById("guest").style.color="white"
+    }
+    else if (count01<count02) {
+        stats.innerHTML = "guest is higher by:";count02-count01
+        document.getElementById("guest").style.color="red"
+        document.getElementById("home").style.color="white"
+    }
+    else if(count01=count02){
+        stats.innerHTML = "Same Score Both"
+        document.getElementById("guest").style.color="white"
+        document.getElementById("home").style.color="white"
+    }
 }
